842ff95f8b2d4e97acff22f3b3f85517c0d1a76b
Alex Vetsavong <avetsa2@illinois.edu>
2020-09-07 04:41:37 -0500

    Pass all covers in grading harness: no errors.

--- mp1/multiplier/hvl/testbench.sv
+++ mp1/multiplier/hvl/testbench.sv
@@ -34,8 +34,9 @@ endtask : reset
 
 task start();
     itf.start <= 1'b1;
-    ##1
+    ##1;
     itf.start <= 1'b0;
+    ##1;
 endtask : start
 
 // error_e defined in package mult_types in file ../include/types.sv
@@ -52,6 +53,7 @@ initial itf.reset_n = 1'b0;
 initial begin
     reset();
     /********************** Your Code Here *****************************/
+    /* Assert start and check product for every possible operand */
     //generate stimuli
     for (int i = 0; i < 9'b100000000; i++) begin
         for (int j = 0; j < 9'b100000000; j++) begin
@@ -60,20 +62,83 @@ initial begin
             itf.multiplier <= j;
             ##1; // wait for values to get loaded
 
-            // start the multiplier
+            // start multiplier
             start();
-            
-            // TODO: begin coverage for run states
-            // checks for the correct product in product_o
+
+            // wait until done to check output product of dut
             @(tb_clk iff (itf.done == 1'b1)); // wait for dut to be in done state
-            assert (itf.product == spec_output(i,j))
-              else begin
-                $error ("%0d: %0t: BAD_PRODUCT error detected", `__LINE__, $time);
-                report_error(BAD_PRODUCT);
-              end
+            assert(itf.product == spec_output(i,j))
+            else begin
+              $error ("%0d: %0t: BAD_PRODUCT error detected", `__LINE__, $time);
+              report_error(BAD_PRODUCT);
+            end
+
             reset();
+
+            assert(itf.rdy == 1'b1)
+            else begin
+                $error ("%0d: %0t: NOT_READY error detected", `__LINE__, $time);
+                report_error(NOT_READY);
+            end
         end
     end
+    /************** End of operand checking section **************/
+
+    /************ Begin asserting start in run states*************/
+    // dummy operands for next two covers
+        itf.multiplicand <= 255;
+        itf.multiplier <= 25;
+        ##1; 
+        start();
+        // assert that start only occurs when not in run state
+        if(itf.mult_op == ADD) begin
+            start();
+            assert(itf.rdy == 1'b0)
+            else begin
+                $error ("%0d: %0t: NOT_READY error detected", `__LINE__, $time);
+                report_error(NOT_READY);
+            end
+        end
+
+        if(itf.mult_op == SHIFT) begin
+            start();
+            assert(itf.rdy == 1'b0) 
+            else begin
+                $error ("%0d: %0t: NOT_READY error detected", `__LINE__, $time);
+                report_error(NOT_READY);
+            end
+        end
+    /************ End asserting start in run states ************/
+
+    /*********** Begin asserting reset in run states ***********/
+    // assert that reset occurs in ADD state
+    itf.multiplicand <= 255;
+    itf.multiplier <= 25;
+    ##1;
+    start();
+    if(itf.mult_op == ADD) begin
+        reset();
+        assert(itf.rdy == 1'b1) 
+        else begin
+            $error ("%0d: %0t: NOT_READY error detected", `__LINE__, $time);
+            report_error(NOT_READY);
+        end
+    end
+
+    @(tb_clk iff itf.rdy == 1'b1);
+
+    // assert that reset occurs in SHIFT state
+    start();
+    if(itf.mult_op == SHIFT) begin 
+        reset();
+        assert(itf.rdy == 1'b1) 
+        else begin
+            $error ("%0d: %0t: NOT_READY error detected", `__LINE__, $time);
+            report_error(NOT_READY);
+        end
+    end
+    /************ End asserting reset in run states ************/
+
     /*******************************************************************/
     itf.finish(); // Use this finish task in order to let grading harness
                   // complete in process and/or scheduled operations
