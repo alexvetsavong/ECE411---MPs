99a33c189eb1767b3d4b35b8fd3f42a4081d92ca
Alex Vetsavong <avetsa2@illinois.edu>
2020-10-11 23:50:48 -0500

    cpu datapath and control added from mp2

--- mp3/hdl/cpu/control.sv
+++ mp3/hdl/cpu/control.sv
@@ -10,6 +10,13 @@ module control
     input logic br_en,
     input logic [4:0] rs1,
     input logic [4:0] rs2,
+
+    /* ports to memory */
+    input logic mem_resp, 
+    output logic mem_read, mem_write,
+    output logic [3:0] mem_byte_enable,
+
+    /* control ports to datapath module */
     output pcmux::pcmux_sel_t pcmux_sel,
     output alumux::alumux1_sel_t alumux1_sel,
     output alumux::alumux2_sel_t alumux2_sel,
@@ -17,6 +24,11 @@ module control
     output marmux::marmux_sel_t marmux_sel,
     output cmpmux::cmpmux_sel_t cmpmux_sel,
     output alu_ops aluop,
+    output branch_funct3_t cmpop,
+
+    /* we're going to use this to figure out r/wmask */
+    input logic [1:0] mask_shift,
+
     output logic load_pc,
     output logic load_ir,
     output logic load_regfile,
@@ -61,8 +73,8 @@ begin : trap_check
         op_load: begin
             case (load_funct3)
                 lw: rmask = 4'b1111;
-                lh, lhu: rmask = 4'bXXXX /* Modify for MP1 Final */ ;
-                lb, lbu: rmask = 4'bXXXX /* Modify for MP1 Final */ ;
+                lh, lhu: rmask = 4'b0011 << (mask_shift) /* Modify for MP1 Final */ ;
+                lb, lbu: rmask = 4'b0001 << (mask_shift) /* Modify for MP1 Final */ ;
                 default: trap = 1;
             endcase
         end
@@ -70,8 +82,8 @@ begin : trap_check
         op_store: begin
             case (store_funct3)
                 sw: wmask = 4'b1111;
-                sh: wmask = 4'bXXXX /* Modify for MP1 Final */ ;
-                sb: wmask = 4'bXXXX /* Modify for MP1 Final */ ;
+                sh: wmask = 4'b0011 << (mask_shift) /* Modify for MP1 Final */ ;
+                sb: wmask = 4'b0001 << (mask_shift) /* Modify for MP1 Final */ ;
                 default: trap = 1;
             endcase
         end
@@ -83,6 +95,15 @@ end
 
 enum int unsigned {
     /* List of states */
+    fetch1, fetch2, fetch3,
+    decode,
+    br, jmp,
+    s_imm, s_reg,
+    s_lui,
+    s_auipc,
+    calc_addr,
+    ld1, ld2,
+    st1, st2
 } state, next_states;
 
 /************************* Function Definitions *******************************/
@@ -104,6 +125,27 @@ enum int unsigned {
  *   and then call it at the beginning of your always_comb block.
 **/
 function void set_defaults();
+    load_pc = 1'b0;
+    load_ir = 1'b0;
+    load_regfile = 1'b0;
+    load_mar = 1'b0;
+    load_mdr = 1'b0;
+    load_data_out = 1'b0;
+    pcmux_sel = pcmux::pc_plus4;
+    cmpop = branch_funct3;
+
+    alumux1_sel = alumux::rs1_out;
+    alumux2_sel = alumux::i_imm;
+    regfilemux_sel = regfilemux::alu_out;
+    marmux_sel = marmux::pc_out;
+    cmpmux_sel = cmpmux::rs2_out;
+    aluop = alu_ops'(arith_funct3);
+
+    mem_read = 1'b0;
+    mem_write = 1'b0;
+
+    mem_byte_enable = 4'b1111;
+
 endfunction
 
 /**
@@ -116,12 +158,18 @@ function void loadPC(pcmux::pcmux_sel_t sel);
 endfunction
 
 function void loadRegfile(regfilemux::regfilemux_sel_t sel);
+    load_regfile = 1'b1;
+    regfilemux_sel = sel;
 endfunction
 
 function void loadMAR(marmux::marmux_sel_t sel);
+    load_mar = 1'b1;
+    marmux_sel = sel;
 endfunction
 
 function void loadMDR();
+    load_mdr = 1'b1;
+    mem_read = 1'b1;
 endfunction
 
 /**
@@ -132,13 +180,16 @@ function void setALU(alumux::alumux1_sel_t sel1,
                                alumux::alumux2_sel_t sel2,
                                logic setop = 1'b0, alu_ops op = alu_add);
     /* Student code here */
-
+    alumux1_sel = sel1;
+    alumux2_sel = sel2;
 
     if (setop)
         aluop = op; // else default value
 endfunction
 
 function automatic void setCMP(cmpmux::cmpmux_sel_t sel, branch_funct3_t op);
+    cmpmux_sel = sel;
+    cmpop = op;
 endfunction
 
 /*****************************************************************************/
@@ -150,17 +201,241 @@ begin : state_actions
     /* Default output assignments */
     set_defaults();
     /* Actions for each state */
+    case(state)
+        /* write case for each state */
+            fetch1: loadMAR(marmux::pc_out);
+            fetch2: loadMDR();
+            fetch3: load_ir = 1'b1;
+            decode: /* no actual code here to be done */;
+            br: 
+            begin
+                setCMP(cmpmux::rs2_out, branch_funct3);
+                setALU(alumux::pc_out, alumux::b_imm, 1'b1, alu_add);
+                loadPC(pcmux::pcmux_sel_t'($unsigned(br_en)));
+            end
+
+            jmp:
+            begin
+                case(opcode)
+                    op_jal: 
+                    begin
+                        loadRegfile(regfilemux::pc_plus4);
+                        /* calculate the jump address*/
+                        setALU(alumux::pc_out, alumux::j_imm, 1'b1, alu_add);
+                        loadPC(pcmux::alu_mod2); 
+                    end
+                    op_jalr:
+                    begin 
+                        loadRegfile(regfilemux::pc_plus4);
+                        /* calculate the jump address */
+                        setALU(alumux::rs1_out, alumux::i_imm, 1'b1, alu_add);
+                        loadPC(pcmux::alu_mod2);
+                    end
+                endcase
+            end
+
+            s_imm: 
+            begin 
+                case(arith_funct3)
+                slt: 
+                begin 
+                    loadRegfile(regfilemux::br_en);
+                    loadPC(pcmux::pc_plus4);
+                    setCMP(cmpmux::i_imm, blt);
+                end 
+                sltu: 
+                begin
+                    loadRegfile(regfilemux::br_en);
+                    loadPC(pcmux::pc_plus4);
+                    setCMP(cmpmux::i_imm, bltu);
+                end
+                sr: 
+                begin
+                    loadRegfile(regfilemux::alu_out);
+                    loadPC(pcmux::pc_plus4);
+                    case(funct7[5])
+                        1'b0 : setALU(alumux::rs1_out, alumux::i_imm, 1'b1, alu_srl);
+                        1'b1 : setALU(alumux::rs1_out, alumux::i_imm, 1'b1, alu_sra);
+                    endcase
+                end
+                default: 
+                begin 
+                    loadRegfile(regfilemux::alu_out);
+                    loadPC(pcmux::pc_plus4);
+                    setALU(alumux::rs1_out, alumux::i_imm, 1'b1, alu_ops'(funct3));
+                end 
+                endcase
+            end
+
+            s_reg: 
+            begin 
+                case(arith_funct3)
+                slt: 
+                begin 
+                    loadRegfile(regfilemux::br_en);
+                    loadPC(pcmux::pc_plus4);
+                    setCMP(cmpmux::rs2_out, blt);
+                end 
+                sltu: 
+                begin
+                    loadRegfile(regfilemux::br_en);
+                    loadPC(pcmux::pc_plus4);
+                    setCMP(cmpmux::rs2_out, bltu);
+                end
+                sr: 
+                begin
+                    loadRegfile(regfilemux::alu_out);
+                    loadPC(pcmux::pc_plus4);
+                    case(funct7[5])
+                        1'b0 : setALU(alumux::rs1_out, alumux::rs2_out, 1'b1, alu_srl);
+                        1'b1 : setALU(alumux::rs1_out, alumux::rs2_out, 1'b1, alu_sra);
+                    endcase
+                end
+                default: 
+                begin 
+                    loadRegfile(regfilemux::alu_out);
+                    loadPC(pcmux::pc_plus4);
+                    setALU(alumux::rs1_out, alumux::rs2_out, 1'b1, alu_ops'(funct3));
+                    if(alu_ops'(funct3) == add) begin
+                        case(funct7[5])
+                        1'b0 : setALU(alumux::rs1_out, alumux::rs2_out, 1'b1, alu_add);
+                        1'b1 : setALU(alumux::rs1_out, alumux::rs2_out, 1'b1, alu_sub);
+                        endcase
+                    end
+                end 
+                endcase
+            end
+
+            s_lui: 
+            begin 
+                loadRegfile(regfilemux::u_imm);
+                loadPC(pcmux::pc_plus4);
+            end
+            s_auipc: 
+            begin 
+                setALU(alumux::pc_out, alumux::u_imm, 1'b1, alu_add);
+                loadRegfile(regfilemux::alu_out);
+                loadPC(pcmux::pc_plus4);
+            end
+            calc_addr:
+            begin
+                case(opcode)
+                    op_load: 
+                    begin
+                        loadMAR(marmux::alu_out); 
+                        setALU(alumux::rs1_out, alumux::i_imm, 1'b1, alu_add);
+                    end
+                    op_store: 
+                    begin
+                        loadMAR(marmux::alu_out);
+                        setALU(alumux::rs1_out, alumux::s_imm, 1'b1, alu_add);
+                        load_data_out = 1'b1;
+                    end
+                endcase
+            end
+            ld1: loadMDR();
+            ld2:
+            begin
+                case(load_funct3)
+                    lw: loadRegfile(regfilemux::lw);
+                    lh: loadRegfile(regfilemux::lh);
+                    lhu: loadRegfile(regfilemux::lhu);
+                    lb: loadRegfile(regfilemux::lb);
+                    lbu: loadRegfile(regfilemux::lbu);
+                endcase
+                loadPC(pcmux::pc_plus4);
+            end
+            st1: 
+            begin 
+                case(store_funct3) 
+                /* TODO: check for longer testing over time */
+                    sb: mem_byte_enable = 4'b0001 << mask_shift;
+                    sh: mem_byte_enable = 4'b0011 << mask_shift;
+                    sw: mem_byte_enable = 4'b1111;
+                endcase
+                mem_write = 1'b1;
+            end
+            st2: loadPC(pcmux::pc_plus4);
+        default: set_defaults();
+    endcase
 end
 
 always_comb
 begin : next_state_logic
     /* Next state information and conditions (if any)
      * for transitioning between states */
+    case(state)
+        fetch1: next_states = fetch2; 
+        fetch2: 
+        begin 
+            if (mem_resp) 
+                next_states = fetch3;
+            else 
+                next_states = fetch2; 
+        end
+        fetch3: next_states = decode;
+        decode: 
+        begin
+            case(opcode)
+                /* figure out next state based on decoded op */
+                op_lui: next_states = s_lui;
+                op_auipc: next_states = s_auipc;
+                op_br: next_states = br;
+                op_load: next_states = calc_addr;
+                op_store: next_states = calc_addr;
+                op_imm: next_states = s_imm;
+
+                /* figure out what state to enter for register-register operations */
+                op_reg: next_states = s_reg;
+                /* checkpoint 2 stuff here */
+                op_jal: next_states = jmp;
+                op_jalr: next_states = jmp;
+                op_csr: next_states = decode;
+                
+                default: next_states = decode;
+            endcase
+        end
+        s_imm: next_states = fetch1;
+        s_reg: next_states = fetch1;
+        s_lui: next_states = fetch1;
+        s_auipc: next_states = fetch1;
+        br: next_states = fetch1;
+
+        jmp: next_states = fetch1;
+
+        calc_addr: 
+        begin
+            case(opcode)
+                op_load: next_states = ld1;
+                op_store: next_states = st1;
+                default: next_states = fetch1;
+            endcase 
+        end
+        ld1: 
+        begin 
+            if(mem_resp) 
+                next_states = ld2; 
+            else next_states = ld1; 
+        end
+        ld2: next_states = fetch1;
+        st1: 
+        begin 
+            if(mem_resp) 
+                next_states = st2; 
+            else next_states = st1; 
+        end
+        st2: next_states = fetch1;
+        default: next_states = state; // by default, stay the same
+    endcase
 end
 
 always_ff @(posedge clk)
 begin: next_state_assignment
     /* Assignment of next state on clock edge */
+    if(rst) begin
+        state <= fetch1;
+    end
+    else state <= next_states;
 end
 
 endmodule : control
--- mp3/hdl/cpu/datapath.sv
+++ mp3/hdl/cpu/datapath.sv
@@ -6,21 +6,71 @@ module datapath
 (
     input clk,
     input rst,
-    input load_mdr,
+
+    /* input from controller */
+    input pcmux::pcmux_sel_t pcmux_sel,
+    input alumux::alumux1_sel_t alumux1_sel,
+    input alumux::alumux2_sel_t alumux2_sel,
+    input regfilemux::regfilemux_sel_t regfilemux_sel,
+    input marmux::marmux_sel_t marmux_sel,
+    input cmpmux::cmpmux_sel_t cmpmux_sel,
+    input alu_ops aluop,
+    input branch_funct3_t cmpop,
+    input logic load_pc,
+    input logic load_ir,
+    input logic load_regfile,
+    input logic load_mar,
+    input logic load_mdr,
+    input logic load_data_out,
+
+    /* datapath ouputs to memory */
     input rv32i_word mem_rdata,
-    output rv32i_word mem_wdata, // signal used by RVFI Monitor
+    output rv32i_word mem_wdata, // signal used by RVFI
+    output rv32i_word mem_address, 
+    
+    /* datapath ouputs to controller module */
+    output logic [1:0] mask_shift,
+    output rv32i_opcode opcode,
+    output logic [2:0] funct3,
+    output logic [6:0] funct7,
+    output logic br_en,
+    output logic [4:0] rs1,
+    output logic [4:0] rs2
 );
 
 /******************* Signals Needed for RVFI Monitor *************************/
 rv32i_word pcmux_out;
 rv32i_word mdrreg_out;
 /*****************************************************************************/
+rv32i_word mem_addr_out;
+assign mask_shift = mem_addr_out[1:0];
 
+store_funct3_t store_funct3;
+assign store_funct3 = store_funct3_t'(funct3);
 
 /***************************** Registers *************************************/
 // Keep Instruction register named `IR` for RVFI Monitor
-ir IR();
+rv32i_word i_imm, s_imm, b_imm, u_imm, j_imm;
+logic [4:0] rd;
+ir IR(
+    .clk (clk),
+    .rst (rst),
+    .load (load_ir),
+    .in (mdrreg_out),
+    .funct3(funct3),
+    .funct7(funct7),
+    .opcode(opcode),
+    .i_imm(i_imm),
+    .s_imm(s_imm),
+    .b_imm(b_imm),
+    .u_imm(u_imm),
+    .j_imm(j_imm),
+    .rs1(rs1),
+    .rs2(rs2),
+    .rd(rd)
+);
 
+// instantiate the registers on the datapath
 register MDR(
     .clk  (clk),
     .rst (rst),
@@ -29,9 +79,86 @@ register MDR(
     .out  (mdrreg_out)
 );
 
+rv32i_word marmux_out;
+assign mem_address = mem_addr_out & 32'hfffffffc;
+register MAR(
+    .clk  (clk),
+    .rst (rst),
+    .load (load_mar),
+    .in   (marmux_out),
+    .out  (mem_addr_out)
+);
+
+// pc register instantiation
+rv32i_word pc_out;
+pc_register pc_reg(
+    .clk(clk),
+    .rst(rst),
+    .load(load_pc),
+    .in(pcmux_out),
+    .out(pc_out)
+);
+
+// instantiate regfile
+rv32i_word rs1_out, rs2_out;
+rv32i_word regfilemux_out;
+
+regfile regfile(
+    .clk (clk),
+    .rst (rst),
+    .load (load_regfile),
+    .in (regfilemux_out),
+    .src_a (rs1),
+    .src_b (rs2),
+    .dest (rd),
+    .reg_a (rs1_out),
+    .reg_b (rs2_out)
+);
+
+/* connect mem_data_out to rs2_out */
+rv32i_word mem_wdata_out;
+register mem_data_out
+(
+    .clk (clk),
+    .rst (rst),
+    .load (load_data_out),
+    .in (rs2_out),
+    .out (mem_wdata_out)
+);
+
 /*****************************************************************************/
+always_comb begin
+    if(opcode == op_store) begin
+        case(store_funct3)
+            sw: mem_wdata = mem_wdata_out;
+            sh, sb: mem_wdata = mem_wdata_out << (mask_shift * 8);
+            default: mem_wdata = mem_wdata_out;
+        endcase
+    end
+    else mem_wdata = mem_wdata_out;
+end
+
 
 /******************************* ALU and CMP *********************************/
+
+logic [31:0] alumux1_out, alumux2_out, alu_out;
+rv32i_word cmp_mux_out;
+
+/* instantiate the ALU and CMP here */
+alu ALU(
+    .aluop(aluop),
+    .a(alumux1_out),
+    .b(alumux2_out),
+    .f(alu_out)
+);
+
+cmp_module CMP(
+    .cmpop(cmpop),
+    .cmp_mux_out(cmp_mux_out),
+    .rs1_out(rs1_out),
+    .br_en(br_en)
+);
+
 /*****************************************************************************/
 
 /******************************** Muxes **************************************/
@@ -39,14 +166,106 @@ always_comb begin : MUXES
     // We provide one (incomplete) example of a mux instantiated using
     // a case statement.  Using enumerated types rather than bit vectors
     // provides compile time type safety.  Defensive programming is extremely
-    // useful in SystemVerilog.  In this case, we actually use 
+    // useful in SystemVerilog.  In this case, we actually use
     // Offensive programming --- making simulation halt with a fatal message
     // warning when an unexpected mux select value occurs
+
     unique case (pcmux_sel)
-        pcmux::pc_plus4: pcumux_out = pc_out + 4;
-        // etc.
+        pcmux::pc_plus4: pcmux_out = pc_out + 4;
+        pcmux::alu_out: pcmux_out = alu_out;
+        /* checkpoint 2 stuff */
+        pcmux::alu_mod2: pcmux_out = alu_out & 32'hfffffffc;
         default: `BAD_MUX_SEL;
     endcase
-end
+
+    unique case(marmux_sel)
+        marmux::pc_out: marmux_out = pc_out;
+        marmux::alu_out: marmux_out = alu_out;
+        default: `BAD_MUX_SEL;
+    endcase
+
+    unique case(cmpmux_sel)
+        cmpmux::rs2_out: cmp_mux_out = rs2_out;
+        cmpmux::i_imm: cmp_mux_out = i_imm;
+        default: `BAD_MUX_SEL;
+    endcase
+
+    unique case(alumux1_sel)
+        alumux::rs1_out: alumux1_out = rs1_out;
+        alumux::pc_out: alumux1_out = pc_out;
+        default: `BAD_MUX_SEL;
+    endcase
+
+    unique case(alumux2_sel)
+        alumux::i_imm: alumux2_out = i_imm;
+        alumux::u_imm: alumux2_out = u_imm;
+        alumux::b_imm: alumux2_out = b_imm;
+        alumux::s_imm: alumux2_out = s_imm;
+        alumux::rs2_out: alumux2_out = rs2_out;
+        /* implement these for checkpoint 2 */
+        alumux::j_imm: alumux2_out = j_imm;
+        default: `BAD_MUX_SEL;
+    endcase
+
+    unique case(regfilemux_sel) 
+        regfilemux::alu_out: regfilemux_out = alu_out;
+        regfilemux::br_en: regfilemux_out = {31'd0, br_en};
+        regfilemux::u_imm: regfilemux_out = u_imm;
+        regfilemux::lw: regfilemux_out = mdrreg_out;
+        /* implement these for checkpoint 2 */
+        regfilemux::pc_plus4: regfilemux_out = pc_out + 4;
+        regfilemux::lh: 
+            begin case(mask_shift)
+                2'b00: regfilemux_out = 32'($signed(mdrreg_out[15:0]));
+                2'b10: regfilemux_out = 32'($signed(mdrreg_out[31:16]));
+                default: regfilemux_out = mdrreg_out;
+            endcase end 
+        regfilemux::lhu: 
+            begin case(mask_shift)
+                2'b00: regfilemux_out = 32'(mdrreg_out[15:0]);
+                2'b10: regfilemux_out = 32'(mdrreg_out[31:16]);
+                default: regfilemux_out = mdrreg_out;
+            endcase end 
+        regfilemux::lb: 
+            begin case(mask_shift)
+                2'b00: regfilemux_out = 32'($signed(mdrreg_out[7:0]));
+                2'b01: regfilemux_out = 32'($signed(mdrreg_out[15:8]));
+                2'b10: regfilemux_out = 32'($signed(mdrreg_out[23:16]));
+                2'b11: regfilemux_out = 32'($signed(mdrreg_out[31:24]));
+            endcase end
+        regfilemux::lbu:             
+            begin case(mask_shift)
+                2'b00: regfilemux_out = 32'(mdrreg_out[7:0]);
+                2'b01: regfilemux_out = 32'(mdrreg_out[15:8]);
+                2'b10: regfilemux_out = 32'(mdrreg_out[23:16]);
+                2'b11: regfilemux_out = 32'(mdrreg_out[31:24]);
+            endcase end
+        default: `BAD_MUX_SEL;
+    endcase
+end : MUXES
+
 /*****************************************************************************/
 endmodule : datapath
+
+/************************************ CMP ************************************/
+module cmp_module
+(
+    input branch_funct3_t cmpop,
+    input rv32i_word rs1_out, // src1
+    input rv32i_word cmp_mux_out, // src2
+    output logic br_en
+);
+
+always_comb begin : br_en_logic
+    case (cmpop)
+        beq: br_en = (rs1_out == cmp_mux_out);
+        bne: br_en = (rs1_out != cmp_mux_out);
+        blt: br_en = ($signed(rs1_out) < $signed(cmp_mux_out));
+        bge: br_en = ($signed(rs1_out) >= $signed(cmp_mux_out));
+        bltu: br_en = (rs1_out < cmp_mux_out);
+        bgeu: br_en = (rs1_out >= cmp_mux_out);
+        default: br_en = 1'b0;
+    endcase
+end : br_en_logic
+
+endmodule : cmp_module
